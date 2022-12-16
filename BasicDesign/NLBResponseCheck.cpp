5ce51475eb66b46be7dba967db1aa8b3f970ee5d
Viet Hung Nguyen <nguyenviethung9900@gmail.com>
2022-12-16 12:07:00 +0900

    概要設計

---
+++ BasicDesign/NLBResponseCheck.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+
+int checkNLBHealth(const char* host, const char* port) {
+  struct addrinfo hints;
+  struct addrinfo *result, *rp;
+  int sfd, s;
+
+  memset(&hints, 0, sizeof(struct addrinfo));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = 0;
+  hints.ai_protocol = 0;
+
+  s = getaddrinfo(host, port, &hints, &result);
+  if (s != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+    return -1;
+  }
+
+  for (rp = result; rp != NULL; rp = rp->ai_next) {
+    sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+    if (sfd == -1) {
+      continue;
+    }
+
+    if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1) {
+      break;
+    }
+
+    close(sfd);
+  }
+
+  if (rp == NULL) {
+    fprintf(stderr, "Could not connect to load balancer\n");
+    return -1;
+  }
+
+  freeaddrinfo(result);
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s host port\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  if (checkNLBHealth(argv[1], argv[2]) == 0) {
+    printf("Load balancer is healthy\n");
+  } else {
+    printf("Load balancer is NOT healthy\n");
+  }
+
+  exit(EXIT_SUCCESS);
+}
